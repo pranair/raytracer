5b29fcf1230e1b94a91bad6a73f88eb5271f94d3
Pratyush Nair <pratyushnair01@gmail.com>
2021-09-09 14:59:45 +0530

    added antialiasing by taking average of multiple samples

--- rtweekend.h
+++ rtweekend.h
@@ -31,10 +31,10 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
-inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
